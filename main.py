90aaaef6f1635250a26523947d50d1e6d3e065ac
seggiepants <41271733+seggiepants@users.noreply.github.com>
2021-06-08 00:15:20 -0600

    Added example markdown file. Put in some resize for the text view, and had H1 text font changed if it matched a regular expression for an H1 in Markdown

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 import tkinter as tk
 from tkinter import filedialog
+from tkinter.font import Font
+
+import re
+
+patterns = {
+    "H1": R"^.+\n=+$"
+    , "H1_": R"^# .+$"
+    }
 
 class Application(tk.Frame):    
     def __init__(self, root=None):
         super().__init__(root)
         self.root = root
-        self.pack()
+        self.fontH1 = Font(family='Helvetica',
+        size=36, weight='bold')
         self.create_widgets()
 
     def create_widgets(self):
@@ -29,8 +38,14 @@ class Application(tk.Frame):
         self.quit.pack(side='bottom')
         """
         self.text = tk.Text(self.root)
+        self.scrollbar = tk.Scrollbar(self.text)
         self.text.insert(tk.INSERT, 'This is a test, this is only a test')
-        self.text.pack()
+
+        # To make the textarea auto resizable
+        self.root.grid_rowconfigure(0, weight=1)
+        self.root.grid_columnconfigure(0, weight=1)
+        self.text.grid(sticky = tk.N + tk.E + tk.S + tk.W)
+        # self.text.pack()
         self.root.geometry('640x480')
         self.root.minsize(width=320, height=240)
         self.root.config(menu=menu)
@@ -48,7 +63,42 @@ class Application(tk.Frame):
             file.close()
             self.text.delete('1.0', tk.END)
             self.text.insert(tk.INSERT, fileText)
+            self.reformat(fileText)
 
+    def reformat(self, buffer):
+        print('reformat')
+        pattern = R"^.+\n=+$"
+        start = 0
+        match = re.search(pattern, buffer[start:], re.MULTILINE)
+        tagCount = 0
+        print(match)
+        while match != None:
+            tagName = f"Tag{tagCount}"
+            indexStart = self.convert_index(buffer, match.start())
+            indexEnd = self.convert_index(buffer, match.end())
+            
+            print(f"{indexStart} -> {indexEnd} : {match.start()} -> {match.end()}")
+            self.text.tag_add(tagName, indexStart, indexEnd)
+            self.text.tag_config(tagName, font=self.fontH1)
+            tagCount = tagCount + 1
+            start = start + match.end()
+            match = re.search(pattern, buffer[start:], re.MULTILINE)
+            print(match)
+            
+    def convert_index(self, string, count):
+        lines = string.splitlines()
+        pos = 0
+        row = 0
+        col = 0
+        for line in lines:
+            row = row + 1
+            if pos + len(line) + 1 >= count:
+                col = count - pos
+                return f"{row}.{col}"
+            else:
+                pos += len(line) + 1
+        return "0.0"
+    
     def command_exit(self):
         self.root.destroy();
 
