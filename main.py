f371c11c963f9c795a1dbecc139d86b69e4a30c4
seggiepants <41271733+seggiepants@users.noreply.github.com>
2021-06-08 23:33:19 -0600

    process the text into a token stream before adding to the text view in an easier to scale manner. Images, and URLs may be a problem later. Now handles h1, and h2 tags. Hopefully more tomorrow.

--- main.py
+++ main.py
@@ -5,16 +5,18 @@ from tkinter.font import Font
 import re
 
 patterns = {
-    "H1": R"^.+\n=+$"
-    , "H1_": R"^# .+$"
+    ("h1", R"^.+\n=+$", lambda s: s.splitlines()[0])
+    , ("h1", R"^# .+$", lambda s: s[2:])
+    , ("h2", R"^.+\n-+$", lambda s: s.splitlines()[0])
+    , ("h2", R"^## .+$", lambda s: s[3:])
     }
 
 class Application(tk.Frame):    
     def __init__(self, root=None):
         super().__init__(root)
         self.root = root
-        self.fontH1 = Font(family='Helvetica',
-        size=36, weight='bold')
+        self.fontH1 = Font(family='Helvetica', size=36, weight='bold')
+        self.fontH2 = Font(family='Helvetica', size=21, weight='bold')
         self.create_widgets()
 
     def create_widgets(self):
@@ -38,13 +40,18 @@ class Application(tk.Frame):
         self.quit.pack(side='bottom')
         """
         self.text = tk.Text(self.root)
-        self.scrollbar = tk.Scrollbar(self.text)
-        self.text.insert(tk.INSERT, 'This is a test, this is only a test')
+        self.vscroll = tk.Scrollbar(self.text, command=self.text.yview, orient=tk.VERTICAL)
+        self.text.yscrollcommand = self.vscroll.set
+        # self.scrollbar.grid(sticky = tk.N + tk.E + tk.S)
 
         # To make the textarea auto resizable
-        self.root.grid_rowconfigure(0, weight=1)
-        self.root.grid_columnconfigure(0, weight=1)
-        self.text.grid(sticky = tk.N + tk.E + tk.S + tk.W)
+        #self.root.grid_rowconfigure(0, weight=1)
+        #self.root.grid_columnconfigure(0, weight=1)
+        #self.text.grid(sticky = tk.N + tk.E + tk.S + tk.W)
+
+        self.vscroll.pack(side="right", fill="y")
+        self.text.pack(side="left", fill="both", expand=True)
+        
         # self.text.pack()
         self.root.geometry('640x480')
         self.root.minsize(width=320, height=240)
@@ -61,30 +68,68 @@ class Application(tk.Frame):
             file = open(fileName, 'r')
             fileText = file.read()
             file.close()
-            self.text.delete('1.0', tk.END)
-            self.text.insert(tk.INSERT, fileText)
+            
             self.reformat(fileText)
 
     def reformat(self, buffer):
-        print('reformat')
-        pattern = R"^.+\n=+$"
-        start = 0
-        match = re.search(pattern, buffer[start:], re.MULTILINE)
-        tagCount = 0
-        print(match)
-        while match != None:
-            tagName = f"Tag{tagCount}"
-            indexStart = self.convert_index(buffer, match.start())
-            indexEnd = self.convert_index(buffer, match.end())
-            
-            print(f"{indexStart} -> {indexEnd} : {match.start()} -> {match.end()}")
-            self.text.tag_add(tagName, indexStart, indexEnd)
-            self.text.tag_config(tagName, font=self.fontH1)
-            tagCount = tagCount + 1
-            start = start + match.end()
-            match = re.search(pattern, buffer[start:], re.MULTILINE)
-            print(match)
-            
+        tokens = [('text', buffer)]
+        for patternRow in patterns:
+            tagName = patternRow[0]
+            pattern = patternRow[1]
+            fnFormat = patternRow[2]            
+            index = 0
+            tagCount = 0
+            while index < len(tokens):
+                textBuffer = tokens[index][1]
+                match = re.search(pattern, textBuffer, re.MULTILINE)            
+                if match != None:
+                    indexStart = match.start() #self.convert_index(textBuffer, match.start())
+                    indexEnd = match.end() #self.convert_index(textBuffer, match.end())
+                    
+                    newTokens = []
+                    start = textBuffer[:indexStart]
+                    if len(start) > 0:
+                        newTokens.append(('text', start))
+                    
+                    middle = fnFormat(textBuffer[indexStart:indexEnd])
+                    if len(middle) > 0:
+                        newTokens.append((tagName, ''))
+                        newTokens.append(('text', middle))
+                        newTokens.append(('/' + tagName, ''))
+                    
+                    end = textBuffer[indexEnd:]
+                    if len(end) > 0:
+                        newTokens.append(('text', end))
+                    tokens = tokens[:index] + newTokens + tokens[index:]                
+                    tokens.pop(index + len(newTokens))
+                else:
+                    #print(f"{indexStart} -> {indexEnd} : {match.start()} -> {match.end()}")
+                    #tagName = f"Tag{tagCount}"
+                    #self.text.tag_add(tagName, indexStart, indexEnd)
+                    #self.text.tag_config(tagName, font=self.fontH1)
+                    #tagCount = tagCount + 1
+                    #start = start + match.end()
+                    #match = re.search(pattern, buffer[start:], re.MULTILINE)
+                    #print(match)
+                    index += 1
+        
+        self.text.delete('1.0', tk.END)
+        # self.text.insert(tk.INSERT, fileText)
+        tags = []
+        for token in tokens:            
+            tokenType = token[0]
+            tokenText = token[1]
+
+            if tokenType == 'text':
+                self.text.insert(tk.INSERT, tokenText, tuple(tags))
+            else:
+                if tokenType[0] == '/':
+                    tags.remove(tokenType[1:])
+                else:
+                    tags.append(tokenType)
+        self.text.tag_config('h1', font=self.fontH1)
+        self.text.tag_config('h2', font=self.fontH2)
+        
     def convert_index(self, string, count):
         lines = string.splitlines()
         pos = 0
